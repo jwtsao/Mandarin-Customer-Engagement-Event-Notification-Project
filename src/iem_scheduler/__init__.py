5dbfb274932cee3a161eaeeb8e4e2133bf419c36
Oscar Tseng <liyent@amazon.com>
2022-11-09 17:12:44 +0000

    changed lambda function handler name

--- src/iem_scheduler/__init__.py
+++ src/iem_scheduler/__init__.py
@@ -3,6 +3,6 @@ from aws_lambda_powertools import Logger
 logger = Logger(service="iem_scheduler")
 
 
-def hello_world(event, context):
+def event_handler(event, context):
     logger.info("Received request")
     return {"message": "Hello World"}
