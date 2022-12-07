c663d9f4f7ece7e4b79fea300dc98d5af814c1a9
Oscar Tseng <liyent@amazon.com>
2022-12-07 09:06:03 +0000

    testing permissions

--- src/iem_scheduler/__init__.py
+++ src/iem_scheduler/__init__.py
@@ -9,14 +9,14 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def event_handler(event, context):
-    logging.info("Received request")
+    logging.debug("Received request")
 
     # Parsing event data from SIM --> SNS
     event = SimParser(event)
-    logging.info(event.sim_ticket_id)
-    logging.info(event.edit_id)
-    logging.info(event.updated_fields)
-    logging.info(event.sim_action)
+    logging.debug(event.sim_ticket_id)
+    logging.debug(event.edit_id)
+    logging.debug(event.updated_fields)
+    logging.debug(event.sim_action)
 
     # Get ticket data or updated fields from SIM
     ticket = IemTicket(event.sim_ticket_id)
--- src/iem_scheduler/sim_reader.py
+++ src/iem_scheduler/sim_reader.py
@@ -17,6 +17,8 @@ AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
 
+print(os.environ)
+
 IemTicket = TypeVar("IemTicket", bound="IemTicket")
 
 
