dd3ec759801b9d04ea71125185e76e5674e92881
Oscar Tseng <liyent@amazon.com>
2022-12-08 04:37:39 +0000

    add print for debugging

--- src/iem_scheduler/__init__.py
+++ src/iem_scheduler/__init__.py
@@ -12,5 +12,13 @@ def event_handler(event, context):
 
     # Get ticket data from SIM
     ticket = IemTicket(event)
+    print(ticket.assigned_engineers)
+    print(ticket.event_date_from)
+    print(ticket.event_date_to)
+    print(ticket.is_action_needed)
+    print(ticket.ticket_id)
+    print("is_support_respurces_changed: {}".format(ticket.is_support_respurces_changed))
+    print("is_event_date_from_changed: {}".format(ticket.is_event_date_from_changed))
+    print("is_event_date_to_changed: {}".format(ticket.is_event_date_to_changed))
 
     return {"status": 200}
