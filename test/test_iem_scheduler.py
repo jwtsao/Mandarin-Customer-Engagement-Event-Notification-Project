15d607da3a98b44e6210af3687efc88d989fc834
Oscar Tseng <liyent@amazon.com>
2022-12-13 06:41:40 +0000

    added some test cases

--- test/test_iem_scheduler.py
+++ test/test_iem_scheduler.py
@@ -1,7 +1,55 @@
-def test_iem_scheduler_importable():
-    # Test if BenderLibSIM can be imported
-    from bender.sim import SIM
+import json
 
-    import iem_scheduler  # noqa: F401
+from bender.sim import SIMIssue
 
-    dir(SIM)
+from iem_scheduler.constants import IemTicketFields
+from iem_scheduler.sim_reader import IemTicket
+
+
+class MockIemTicket(IemTicket):
+    def __init__(self):
+        pass
+
+
+class TestSimReader:
+    f = open("test/test_data/ticket_data.json")
+    TESTING_TICKET_JSON = json.load(f)
+    sim_testing_data = SIMIssue(TESTING_TICKET_JSON)
+
+    reader = MockIemTicket()
+    reader._ticket = sim_testing_data
+
+    def test_get_mand_iem_alias_id_by_simissue(self):
+        expected_alias = "MAND-IEM-546"
+        alias = self.reader._get_mand_iem_alias_id_by_simissue(self.reader._ticket)
+
+        assert alias == expected_alias
+
+    def test_parse_nominated_support_resources(self):
+        custom_fields = self.reader._ticket.custom_fields[
+            IemTicketFields.NOMINATED_SUPPORT_RESOURCES.split("/")[-1]
+        ]
+
+        expected_support_resources = [
+            {
+                "login": "liyent",
+                "start time": "12/08/2022 08:00 AM",
+                "end time": "12/08/2022 12:00 AM",
+                "profile": "SCD",
+            },
+            {
+                "login": "werfserf",
+                "start time": "12/08/2022 08:00 AM",
+                "end time": "12/08/2022 12:00 AM",
+                "profile": "Networking",
+            },
+            {
+                "login": "werfrf",
+                "start time": "12/08/2022 08:00 AM",
+                "end time": "12/08/2022 12:00 AM",
+                "profile": "Networking",
+            },
+        ]
+        support_resources = self.reader._parse_nominated_support_resources(custom_fields)
+
+        assert support_resources == expected_support_resources
