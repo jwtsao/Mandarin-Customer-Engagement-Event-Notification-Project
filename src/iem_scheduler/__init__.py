82202367c2b62cafa31affa3de5c7ff1e48cbe9d
Oscar Tseng <liyent@amazon.com>
2022-12-14 11:37:57 +0000

    push testing code

--- src/iem_scheduler/__init__.py
+++ src/iem_scheduler/__init__.py
@@ -20,32 +20,24 @@ def event_handler(event, context):
 
     if ticket.is_action_needed:
         engineers = ticket.assign_engineers
-        if ticket.is_create:
-            db = DynamodbIEM()
-            for entry in engineers:
-                db.write(
-                    ticket_id,
-                    entry["login"],
-                    entry["start time"],
-                    entry["end time"],
-                    entry["profile"],
-                )
-                event_details = db.read(ticket_id, entry["login"])
-                new_event = EventBridge()
-                event_name = entry["login"] + "_" + ticket_id
-                event_time = new_event.timeExpressionEditor(entry["start time"])
-                new_event.eventScheduler(event_name, event_time, event_details)
+        # comment out the below if statement for testing
+        # if ticket.is_create:
+        db = DynamodbIEM()
+        for entry in engineers:
+            db.write(
+                ticket_id,
+                entry["login"],
+                entry["start time"],
+                entry["end time"],
+                entry["profile"],
+            )
+            event_details = db.read(ticket_id, entry["login"])
+            new_event = EventBridge()
+            event_name = entry["login"] + "_" + ticket_id
+            event_time = new_event.timeExpressionEditor(entry["start time"])
+            new_event.eventScheduler(event_name, event_time, event_details)
     #     else:
     #         # ticket editing
     #         pass  # TODO
-
-    print(ticket.assigned_engineers)
-    print(ticket.event_date_from)
-    print(ticket.event_date_to)
-    print(ticket.is_action_needed)
-    print(ticket.ticket_id)
-    print("is_support_respurces_changed: {}".format(ticket.is_support_respurces_changed))
-    print("is_event_date_from_changed: {}".format(ticket.is_event_date_from_changed))
-    print("is_event_date_to_changed: {}".format(ticket.is_event_date_to_changed))
 
     return {"status": 200}
